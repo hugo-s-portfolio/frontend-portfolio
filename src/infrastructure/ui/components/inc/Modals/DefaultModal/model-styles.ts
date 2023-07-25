d226bfb6f58915e4f398b943de9b86be65a2abc5
hadiazb <hadiazb@unal.edu.co>
2023-07-24 22:24:50 -0500

    fix the lint bugs in the CArdMediaProps

--- src/infrastructure/ui/components/inc/Modals/DefaultModal/model-styles.ts
+++ src/infrastructure/ui/components/inc/Modals/DefaultModal/model-styles.ts
@@ -5,13 +5,13 @@ import { StyledShadowMixed } from '@/infrastructure/ui/styles'
 
 export const StyledModal = styled(Modal)`
     .MuiBackdrop-root {
-        ${tw`bg-dark !opacity-50`}
+        ${tw`bg-blend-darken !opacity-50`}
     }
 `
 
 export const StyledBox = styled(Box)`
     ${StyledShadowMixed}
-    ${tw`absolute top-[50%] left-[50%] p-5 bg-light  min-w-[400px] rounded`}
+    ${tw`absolute top-[50%] left-[50%] p-5 bg-blend-darken  min-w-[400px] rounded`}
 
     transform: translate(-50%, -50%);
 `
