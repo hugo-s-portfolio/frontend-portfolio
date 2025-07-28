e5280cfbbac40374d66fc5dfd82c1dbf9fdeddcb
hadiazb <hugo.diaz@bitsamericas.com>
2025-07-28 15:19:52 -0500

    Add icons fix

--- src/infrastructure/ui/utils/icons/index.ts
+++ src/infrastructure/ui/utils/icons/index.ts
@@ -1,1 +1,8 @@
+import { JSXElementConstructor, ReactElement } from 'react'
+
+export type IIcons = Record<
+    string,
+    string | ReactElement<unknown, string | JSXElementConstructor<unknown>>
+>
+
 export * from './getMUIcons'
