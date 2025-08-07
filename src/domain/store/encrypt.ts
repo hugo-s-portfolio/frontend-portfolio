e867f72f4c6995ce3ceb40dc364dd745a7abc1dd
hadiazb <hugo.diaz@bitsamericas.com>
2025-08-07 11:03:52 -0500

    create emvironments files in the nextjs front project

--- src/domain/store/encrypt.ts
+++ src/domain/store/encrypt.ts
@@ -1,7 +1,7 @@
 import { encryptTransform } from 'redux-persist-transform-encrypt'
 
 export const encrypt = encryptTransform({
-    secretKey: process.env.NEXT_PUBLIC_SECRET_STORE ?? ' ',
+    secretKey: process.env.NEXT_PUBLIC_SECRET_PORTFOLIO ?? ' ',
     onError: (err) => {
         console.error(err)
     },
